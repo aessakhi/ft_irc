71558cbe844d819e103ea183967c202180a9b444
ldesnoye <ldesnoye@student.42.fr>
2023-02-10 15:33:09 +0100

    port is now a string

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -6,14 +6,14 @@
 /*   By: ldesnoye <ldesnoye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 21:34:26 by aessakhi          #+#    #+#             */
-/*   Updated: 2023/02/10 13:03:39 by ldesnoye         ###   ########.fr       */
+/*   Updated: 2023/02/10 15:09:53 by ldesnoye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
-Server::Server(int port, std::string password): _port(port), _password(password){}
+Server::Server(std::string port, std::string password): _port(port), _password(password)
+{}
 
 Server::~Server()
-{
-}
+{}
