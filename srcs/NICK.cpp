2f0ce5bb0abeedd7554057fff68ae07064e74739
Alexandre ESSAKHI <aessakhi@e2r7p19.clusters.42paris.fr>
2023-02-16 16:17:49 +0100

    Basic nick function

--- srcs/NICK.cpp
+++ srcs/NICK.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   nick.cpp                                           :+:      :+:    :+:   */
+/*   NICK.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aessakhi <aessakhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 15:56:39 by aessakhi          #+#    #+#             */
-/*   Updated: 2023/02/16 16:06:06 by aessakhi         ###   ########.fr       */
+/*   Updated: 2023/02/16 16:17:05 by aessakhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 void	nick(Server *srv, int &userfd, Command &cmd)
 {
+	std::string	client;
+
+	client = "New user";
 	if (cmd.getParamList().empty())
-		srv->sendReply(userfd, "431 :No Nickname given");
+		srv->sendReply(userfd, ERR_NONICKNAMEGIVEN(client));
 	/* Need to add the check against the user list */
 	else
 	{
-		srv->getUser(userfd)->setNickname(cmd.getParamList[0]);
+		srv->getUser(userfd)->setNickname(cmd.getParamList()[0]);
 		std::cout << "Nickname succesfully added" << std::endl;
 	}
 }
