e486c29c5f7d5c2d385d02a6d8edb10f0db583b9
ldesnoye <ldesnoye@student.42.fr>
2023-02-21 17:31:19 +0100

    added comments for my understanding

--- inc/User.hpp
+++ inc/User.hpp
@@ -8,47 +8,91 @@ class User
 private:
 	User();
 
+	/* The fd to use for communication with the client */
 	int			_fd;
 
 	/* These 3 bool will be used to check the authentification of the user */
 	/* For instance, if the password is empty, we'll still accept the connection of the user but won't send the reply msg until these 3 bools are set to true */
+	
+	/* If true, user field has been set */
 	bool		_user;
+
+	/* If true, correct password has been given */
 	bool		_pwd;
+
+	/* If true, nick field has been set */
 	bool		_nick;
 
+	/*  */
 	bool		_authenticated;
 
+	/* User's nickname */
 	std::string	_nickname;
 
+	/* User's username */
 	std::string	_username;
+
+	/* User's hostname */
 	std::string	_hostname;
+
+	/* User's real name */
 	std::string	_realname;
 
+	/* If true, the user is invisible */
+	bool	_is_invisible;
+
+	/* If true, the user is a server operator */
+	bool	_is_operator;
+
+	/* If true, the user is WALLOPS */
+	bool	_is_wallops;
+
 public:
+
 	User(int fd);
 
 	~User();
 
+	/*  */
 	bool	getAuth();
 
+	/* Returns if the user has given the correct password */
 	bool	getPwd();
+
+	/* Returns if the user has set his nickname */
 	bool	getNick();
+
+	/* Returns if the user has set his username */
 	bool	getUser();
 
+	/* Returns nickname */
 	std::string	getNickname() const;
+
+	/* Returns real name */
 	std::string	getRealname() const;
 
-
+	/* Sets _authenticated value */
 	void	setAuth(bool state);
 
+	/* Sets _pwd value */
 	void	setPwd(bool state);
+
+	/* Sets _nick value */
 	void	setNick(bool state);
+
+	/* Sets _user value */
 	void	setUser(bool state);
 
+	/* Returns true if _pwd, _nick, _user are true */
 	bool	checkAuth();
 
+	/* Sets user's nickname */
 	void	setNickname(std::string nickname);
+
+	/* Sets user's username */
 	void	setUsername(std::string username);
+
+	/* Sets user's realname */
 	void	setRealname(std::string realname);
 
 };
