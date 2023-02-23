d817b20e0d4a51bead109f830be2f47ee7023856
Alexandre ESSAKHI <aessakhi@e2r7p21.clusters.42paris.fr>
2023-02-23 14:58:16 +0100

    Changed the msg sent in NICK, USER and PASS, this is just a bandaid solution until we found a more coherent way of sending numeric replies from the serv

--- srcs/PASS.cpp
+++ srcs/PASS.cpp
@@ -23,6 +23,6 @@ void	pass(Server *srv, int &userfd, Command &cmd)
 	if (srv->getUser(userfd)->checkAuth() == true && srv->getUser(userfd)->getAuth() == false)
 	{
 		srv->getUser(userfd)->setAuth(true);
-		srv->sendReply(userfd, RPL_WELCOME(srv->getUser(userfd)->getNickname()));
+		srv->sendReply(userfd, ":" + srv->getHostname() + " " + RPL_WELCOME(srv->getUser(userfd)->getNickname()));
 	}
 }
--- srcs/USER.cpp
+++ srcs/USER.cpp
@@ -24,6 +24,6 @@ void	user(Server *srv, int &userfd, Command &cmd)
 	if (srv->getUser(userfd)->checkAuth() == true && srv->getUser(userfd)->getAuth() == false)
 	{
 		srv->getUser(userfd)->setAuth(true);
-		srv->sendReply(userfd, RPL_WELCOME(srv->getUser(userfd)->getNickname()));
+		srv->sendReply(userfd, ":" + srv->getHostname() + " " + RPL_WELCOME(srv->getUser(userfd)->getNickname()));
 	}
 }
