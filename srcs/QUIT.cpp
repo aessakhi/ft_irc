0aebffdf023138b88cce44cbfa81060abc300966
ldesnoye <ldesnoye@student.42.fr>
2023-03-09 15:01:21 +0100

    QUIT parts user from alll channels and sends ERROR reply

--- srcs/QUIT.cpp
+++ srcs/QUIT.cpp
@@ -1,11 +1,25 @@
 #include "main.hpp"
 
-//GO THROUGH ALL CHANNELS AND PART ALL IN THE CLASS METHOD
 //Connecion to localhost established, but user not welcomed then still receives arg
 
 void	quit(Server *srv, int &userfd, Command &cmd)
 {
-	(void)cmd;
+	User * user = srv->getUser(userfd);
+	std::string reason;
+
+	if (cmd.paramNumber() >= 1)
+		reason = cmd.getLastParam();
+
+	srv->sendReply(userfd, ":" + srv->getName() + " ERROR :Quitting");
+
+	std::map<std::string, Channel *> channels = *(srv->getChannelMap());
+	std::map<std::string, Channel *>::iterator it = channels.begin();
+	std::map<std::string, Channel *>::iterator ite = channels.end();
+	for (; it != ite; it++)
+	{
+		if (!it->second->part(user))
+			it->second->sendToAllMembers(":" + user->getMask() + " QUIT :Quit: " + reason);
+	}
+
 	srv->_removeUserfromServer(userfd);
-	//ERROR MESSAGE TO BE DONE
 }
