8c441b32b54bc02ed0b48e88598ad58c2c5773e3
ldesnoye <ldesnoye@student.42.fr>
2023-03-09 11:16:56 +0100

    added JOIN replies if error

--- srcs/JOIN.cpp
+++ srcs/JOIN.cpp
@@ -26,37 +26,33 @@ void	join(Server *srv, int &userfd, Command &cmd)
 	user = srv->getUser(userfd);
 	if (cmd.getParamList().empty())
 		std::cout << "Need more params" << std::endl;
-	/* Like with PRIVMSG will need to split the targets to send it to each individual targets */
-	/* Need verification if the target starts with '#', shouldn't happen with irssi but someone might try it with nc */
 	channels = splitTargets(cmd.getParam(0), ",");
 	for (std::vector<std::string>::const_iterator it = channels.begin(); it != channels.end(); it++)
 	{
 		if (srv->getChannel(*it) == NULL)
 			srv->getChannelMap()->insert(std::make_pair(*it, new Channel(*it)));
-		err = srv->getChannel(*it)->join(user, cmd.getLastParam());
-		switch(err)
+		Channel * channel = srv->getChannel(*it);
+		err = channel->join(user, cmd.getLastParam());
+		switch (err)
 		{
 			case err_badchannelkey:
-				std::cout << "Bad chan key" << std::endl;
+				srv->sendReply(userfd, ERR_BADCHANNELKEY(user->getNickname(), *it));
 				break;
 			case err_bannedfromchan:
-				std::cout << "Banned" << std::endl;
+				srv->sendReply(userfd, ERR_BANNEDFROMCHAN(user->getNickname(), *it));
 				break;
 			case err_channelisfull:
-				std::cout << "Chan full" << std::endl;
+				srv->sendReply(userfd, ERR_CHANNELISFULL(user->getNickname(), *it));
 				break;
 			case err_inviteonlychan:
-				std::cout << "Invite only Chan" << std::endl;
+			{
+				srv->sendReply(userfd, ERR_INVITEONLYCHAN(user->getNickname(), *it));
 				break;
+			}
 			default:
 			{
-				int targetfd;
-				std::vector<User *> userlist = srv->getChannel(*it)->getUsers();
-				for (std::vector<User *>::const_iterator user_it = userlist.begin(); user_it != userlist.end(); user_it++)
-				{
-					targetfd = srv->getUserfd((*user_it)->getNickname());
-					srv->sendReply(targetfd, ":" + user->getMask() + " JOIN " + cmd.getParam(0));
-				}
+				std::string msg = ":" + user->getMask() + " JOIN " + cmd.getParam(0);
+				channel->sendToAllMembers(msg);
 				break;
 			}
 		}
