e53af19e5a61649b52c45ffd492c210192d2ae61
ldesnoye <ldesnoye@student.42.fr>
2023-03-15 13:41:32 +0100

    fixed segfault on WHOWAS without parameters

--- srcs/cmds/WHOWAS.cpp
+++ srcs/cmds/WHOWAS.cpp
@@ -4,6 +4,12 @@ void	whowas(Server *srv, int &userfd, Command &cmd)
 { //IDK if will do or not but on ERR from WHOIS, client SEND to WHOWAS
 	User * user = srv->getUser(userfd);
 
+	if (cmd.paramNumber() == 0)
+	{
+		srv->sendReply(userfd, ERR_NEEDMOREPARAMS(user->getNickname(), cmd.getCmd()));
+		return;
+	}
+
 	srv->sendReply(userfd, ERR_NOSUCHNICK(user->getNickname(), cmd.getParam(0)));
 	srv->sendReply(userfd, RPL_ENDOFWHOWAS(user->getNickname(), cmd.getParam(0)));
 }
