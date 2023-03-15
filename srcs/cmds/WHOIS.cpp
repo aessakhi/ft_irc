34ec394b7018ba954cefd70aff223582f40e389b
ldesnoye <ldesnoye@student.42.fr>
2023-03-15 13:34:00 +0100

    fixed segfault on errors in WHOIS

--- srcs/cmds/WHOIS.cpp
+++ srcs/cmds/WHOIS.cpp
@@ -19,20 +19,47 @@ void	whois(Server *srv, int &userfd, Command &cmd)
 {
 	User * user = srv->getUser(userfd);
 
-	std::string Nickname = cmd.getParam(0);
-	User * target = srv->getUserbyNickname(Nickname);
+	if (cmd.paramNumber() == 0)
+	{
+		srv->sendReply(userfd, ERR_NONICKNAMEGIVEN(user->getNickname()));
+		return;
+	}
+
+	std::string nickname;
+
+	if (cmd.paramNumber() == 1)
+		nickname = cmd.getParam(0);
+	else
+		nickname = cmd.getParam(1);
+
+	User * target = srv->getUserbyNickname(nickname);
+
+	if (target == NULL)
+	{
+		srv->sendReply(userfd, ERR_NOSUCHNICK(user->getNickname(), nickname));
+		return;
+	}
 
 	std::stringstream creatime;
 	creatime << target->getCreatime();
 
-	if (cmd.paramNumber() != 1 || !target)
-		srv->sendReply(userfd, ERR_NOSUCHNICK(user->getNickname(), Nickname));
-	else
-	{
-		srv->sendReply(userfd, RPL_WHOISUSER(user->getNickname(), Nickname, target->getUsername(), target->getHostname(), target->getRealname()));
-		srv->sendReply(userfd, RPL_WHOISIDLE(user->getNickname(), Nickname, time_idle(target->getIdletime()), creatime.str()));
-		if (target->isAway())
-			srv->sendReply(userfd, RPL_AWAY(user->getNickname(), Nickname, target->getAwayMessage()));
-	}	
-	srv->sendReply(userfd, RPL_ENDOFWHOIS(user->getNickname(), Nickname));
+	srv->sendReply(userfd, RPL_WHOISUSER(user->getNickname(), nickname, target->getUsername(), target->getHostname(), target->getRealname()));
+	srv->sendReply(userfd, RPL_WHOISIDLE(user->getNickname(), nickname, time_idle(target->getIdletime()), creatime.str()));
+	if (target->isOperator())
+		srv->sendReply(userfd, RPL_WHOISOPERATOR(user->getNickname(), nickname));
+	if (target->isAway())
+		srv->sendReply(userfd, RPL_AWAY(user->getNickname(), nickname, target->getAwayMessage()));
+	srv->sendReply(userfd, RPL_ENDOFWHOIS(user->getNickname(), nickname));
 }
+
+/*
+	possible replies to add:
+
+    ~RPL_WHOISSERVER (312)
+    RPL_WHOISCHANNELS (319)
+    RPL_WHOISSPECIAL (320)
+    RPL_WHOISACTUALLY (338)
+    RPL_WHOISHOST (378)
+    RPL_WHOISMODES (379)
+    ~RPL_WHOISSECURE (671)
+*/
