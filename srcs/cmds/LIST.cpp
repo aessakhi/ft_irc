059026ee1fae52e13252e5af35ca024b443048dd
Antoine BARBIER <antbarbi@e2r9p12.clusters.42paris.fr>
2023-03-16 13:33:20 +0100

    /LIST >[param] done

--- srcs/cmds/LIST.cpp
+++ srcs/cmds/LIST.cpp
@@ -43,11 +43,29 @@ void	list_select(Server *srv, int &userfd, Command &cmd, User *user)
 					cpt_memb(it->second), it->second->getTopic()));
 }
 
+void	list_all_users(Server *srv, int &userfd, Command &cmd, User *user)
+{
+	std::map<std::string, Channel *>::iterator it;
+
+	std::string res = cmd.getParam(0).substr(1, 15);
+	if (res.find_first_not_of("0123456789") != std::string::npos)
+		return ;
+
+	srv->sendReply(userfd, RPL_LISTSTART(user->getNickname()));
+
+	size_t cpt = atoi(res.c_str());
+	std::cout << cpt << std::endl;
+	
+	it = srv->getChannelMap()->begin();
+	for (; it != srv->getChannelMap()->end(); it++)
+		if (!it->second->secretMode() && number_of_memb(it->second) > cpt)
+			srv->sendReply(userfd, RPL_LIST(user->getNickname(), it->first, cpt_memb(it->second), it->second->getTopic()));
+	srv->sendReply(userfd, RPL_LISTEND(user->getNickname()));
+}
+
 void	list(Server *srv, int &userfd, Command &cmd)
 {
 	User * user = srv->getUser(userfd);
-
-
 
 	if (cmd.paramNumber() == 0)
 	{
@@ -60,5 +78,7 @@ void	list(Server *srv, int &userfd, Command &cmd)
 		srv->sendReply(userfd, RPL_LISTSTART(user->getNickname()));
 		list_select(srv, userfd, cmd, user);
 		srv->sendReply(userfd, RPL_LISTEND(user->getNickname()));
-	}	
+	}
+	else if (cmd.getParam(0).at(0) == '>')
+		list_all_users(srv, userfd, cmd, user);
 }
