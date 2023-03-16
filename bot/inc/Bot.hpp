e0b40dd3545ce80e9c633d5c3a5126190c5c092d
ldesnoye <ldesnoye@student.42.fr>
2023-03-16 19:39:13 +0100

    separated functions into utils.cpp

--- bot/inc/Bot.hpp
+++ bot/inc/Bot.hpp
@@ -1,29 +1,7 @@
 #ifndef BOT_HPP
 #define BOT_HPP
 
-#include <string>
-#include <stdexcept>
-#include <iostream>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include "colors.hpp"
-#include <stdlib.h>
-#include <signal.h>
-#include <vector>
-#include <fcntl.h>
-#include <errno.h>
-
-#define RECV_BUFFER_SIZE 512
-
-void			printError(std::string s);
-std::string		no_crlf(std::string s);
-void			printReply(std::string s);
-void			printRecv(char * s, size_t n);
-void			printRecv(std::string s, size_t n);
+#include "main.hpp"
 
 class SocketException : public std::exception
 {
--- bot/srcs/bot_main.cpp
+++ bot/srcs/bot_main.cpp
@@ -1,54 +1,10 @@
-#include "Bot.hpp"
+#include "main.hpp"
 
 bool loop = true;
 
 void	sighandler(int)
 {
 	loop = false;
-}
-
-void	printError(std::string s)
-{
-	std::cerr << RED << s << RESET << std::endl;
-}
-
-void	printReply(std::string s)
-{
-	std::cout << BBLU << ">> \"" << s << "\"" << RESET << std::endl;
-}
-
-void	printRecv(char * s, size_t n)
-{
-	std::cout << std::endl << BLU << "<< \"";
-	if (n >= 2)
-	{
-		if (s[n - 2] == '\r' && s[n - 1] == '\n')
-			n -= 2;
-		else if (s[n - 1] == '\n' || s[n - 1] == '\r')
-			n--;
-	}
-	if (n == 1 && (s[0] == '\n' || s[0] == '\r'))
-		n--;
-	
-	for (size_t i = 0; i < n; i++)
-		std::cout << s[i];
-	std::cout << "\"" << RESET << std::endl;
-}
-
-std::string	no_crlf(std::string s)
-{
-	size_t max = s.size();
-	if (s[max - 2] == '\r' && s[max - 1] == '\n')
-		return s.substr(0, max - 2);
-	if (s[max - 1] == '\r' || s[max - 1] == '\n')
-		return s.substr(0, max - 1);
-	return s;
-}
-
-void	printRecv(std::string s, size_t n)
-{
-	std::string	to_print(s, 0, n);
-	std::cout << BBLU << "<< \"" << no_crlf(to_print) << "\"" << RESET << std::endl;
 }
 
 static bool _error_check(int ac)
