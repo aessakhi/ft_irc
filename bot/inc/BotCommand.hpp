9bc76856d858d45529c305e04c42aa6b1746cd35
ldesnoye <ldesnoye@student.42.fr>
2023-03-21 19:35:37 +0100

    added parseMask function

--- bot/inc/BotCommand.hpp
+++ bot/inc/BotCommand.hpp
@@ -7,16 +7,22 @@ class BotCommand
 {
 	public:
 		BotCommand();
-		BotCommand(std::string from_nick, std::string original_target, std::string command, std::string raw_args);
+		BotCommand(std::string from_mask, std::string original_target, std::string command, std::string raw_args);
 		~BotCommand();
 		BotCommand(BotCommand const & src);
 		BotCommand & operator=(BotCommand const & src);
 
-		std::string	from_nick;
+		std::string	from_mask;
 		std::string	original_target;
 		std::string	command;
 		std::string	raw_args;
-		std::vector<std::string> args;
+		std::vector<std::string>	args;
+
+		void	parse_mask();
+
+		std::string	from_nick;
+		std::string	from_user;
+		std::string	from_host;
 };
 
 #endif
--- bot/srcs/BotCommand.cpp
+++ bot/srcs/BotCommand.cpp
@@ -3,8 +3,10 @@
 BotCommand::BotCommand()
 {}
 
-BotCommand::BotCommand(std::string from_nick, std::string original_target, std::string command, std::string raw_args) : from_nick(from_nick), original_target(original_target), command(command), raw_args(raw_args)
-{}
+BotCommand::BotCommand(std::string from_mask, std::string original_target, std::string command, std::string raw_args) : from_mask(from_mask), original_target(original_target), command(command), raw_args(raw_args)
+{
+	parse_mask();
+}
 
 BotCommand::~BotCommand()
 {}
@@ -14,11 +16,31 @@ BotCommand::BotCommand(BotCommand const & src)
 
 BotCommand & BotCommand::operator=(BotCommand const & src)
 {
-	from_nick = src.from_nick;
+	from_mask = src.from_mask;
 	original_target = src.original_target;
 	command = src.command;
 	raw_args = src.raw_args;
 	args = src.args;
 
 	return *this;
+}
+
+void BotCommand::parse_mask()
+{
+	size_t	start = 0;
+	size_t	end = from_mask.find('!', start);
+	
+	if (end == std::string::npos)
+		return ;
+
+	from_nick = from_mask.substr(start, end);
+
+	start = end + 1;
+	end = from_mask.find('@', start);
+
+	from_user = from_mask.substr(start, end);
+
+	start = end + 1;
+
+	from_host = from_mask.substr(start);
 }
